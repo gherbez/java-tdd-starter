75dc67c9083f8813c60198d0e765965111386900
Gregory HERBEZ <gregory.herbez@accenture.com>
2020-05-06 08:10:10 +0200

    push repo

--- src/main/java/RomanNumeral.java
+++ src/main/java/RomanNumeral.java
@@ -1,6 +1,71 @@
+import java.util.List;
+
 public class RomanNumeral {
 
+    final static List<Integer> ARABIC_NUMBER = List.of(100,90,50,40,10 ,9, 5 ,4 ,1);
+    final static List<String> ROMAN_NUMBER = List.of("C", "XC", "L", "XL", "X", "IX", "V", "IV", "I");
+
     public static String convert(int arabicNumber) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        int reamainingArabicNumber = arabicNumber;
+
+
+//        if(arabicNumber != 0 && arabicNumber % 10 == 0){
+//             result.append("XX");
+//             return result.toString();
+//         }
+//         else {
+//             for(int i = 0; i < arabicNumber; i++){
+//                 result.append(("I"));
+//             }
+//             return result.toString();
+//         }
+//        return "";
+
+
+
+        for (int i = 0; i < ARABIC_NUMBER.size(); i++) {
+            if (arabicNumber >= ARABIC_NUMBER.get(i)) {
+                result.append(ROMAN_NUMBER.get(i));
+            }
+        }
+        return result.toString();
+
     }
+//
+//
+//         if (basedMap.containsKey(arabicNumber)) {
+//             return result.append(basedMap.get(arabicNumber)).toString();
+
+//         }else if(arabicNumber != 0 && arabicNumber % 10 == 0){
+//             result.append("XX");
+//             return result.toString();
+//         }
+//         else {
+//             for(int i = 0; i < arabicNumber; i++){
+//                 result.append(("I"));
+//             }
+//             return result.toString();
+//         }
+//        return "";
+
+
 }
+
+
+
+//    private static final Map<Integer , String> basedMap = new HashMap<Integer, String>();
+
+
+//    static {
+//         basedMap.put(1,"I");
+//         basedMap.put(4,"IV");
+//         basedMap.put(5,"V");
+//         basedMap.put(9,"IX");
+//         basedMap.put(10,"X");
+//         basedMap.put(40,"XL");
+//         basedMap.put(50,"L");
+//         basedMap.put(90,"XC");
+//         basedMap.put(100,"C");
+//     }
+
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -8,4 +8,63 @@ public class RomanNumeralTest {
         assertThat(RomanNumeral.convert(0)).isEqualTo("");
     }
 
+    @Test
+    public void shouldReturnIWhenArabicNumberIs1(){
+        assertThat(RomanNumeral.convert(1)).isEqualTo("I");
+    }
+
+    @Test
+    public void shouldReturnVWhenArabicNumberIs5(){
+        assertThat(RomanNumeral.convert(5)).isEqualTo("V");
+    }
+
+    @Test
+    public void shouldReturnVWhenArabicNumberIs10(){
+        assertThat(RomanNumeral.convert(10)).isEqualTo("X");
+    }
+
+    @Test
+    public void shouldReturnVWhenArabicNumberIs50(){
+        assertThat(RomanNumeral.convert(50)).isEqualTo("L");
+    }
+
+    @Test
+    public void shouldReturnVWhenArabicNumberIs100(){
+        assertThat(RomanNumeral.convert(100)).isEqualTo("C");
+    }
+
+    @Test
+    public void shouldReturnIIWhenArabicNumberIs2(){
+        assertThat(RomanNumeral.convert(2)).isEqualTo("II");
+    }
+
+    @Test
+    public void shouldReturnIIIWhenArabicNumberIs3(){
+        assertThat(RomanNumeral.convert(3)).isEqualTo("III");
+    }
+
+    @Test
+    public void shouldReturnXXWhenArabicNumberIs20(){
+        assertThat(RomanNumeral.convert(20)).isEqualTo("XX");
+    }
+
+    @Test
+    public void shouldReturnIVWhenArabicNumberIs4(){
+        assertThat(RomanNumeral.convert(4)).isEqualTo("IV");
+    }
+
+    @Test
+    public void shouldReturnIXWhenArabicNumberIs9(){
+        assertThat(RomanNumeral.convert(9)).isEqualTo("IX");
+    }
+
+    @Test
+    public void shouldReturnXLWhenArabicNumberIs40(){
+        assertThat(RomanNumeral.convert(40)).isEqualTo("XL");
+    }
+
+    @Test
+    public void shouldReturnXCWhenArabicNumberIs90(){
+        assertThat(RomanNumeral.convert(90)).isEqualTo("XC");
+    }
 }
